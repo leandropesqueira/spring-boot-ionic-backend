db4ca4f8d65b45255ac727dedcbb5167c7396a35
leandropesqueira <leandropesqueira7@gmail.com>
2021-09-28 10:27:27 -0400

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/leandrolopes/cursomc/domain/Estado.java
+++ src/main/java/com/leandrolopes/cursomc/domain/Estado.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Estado implements Serializable {
 	
@@ -21,6 +23,7 @@ public class Estado implements Serializable {
 	private Integer id;
 	private String nome;
 	
+	@JsonBackReference // omitido na serializacao
 	@OneToMany(mappedBy= "estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
---
+++ src/main/java/com/leandrolopes/cursomc/resources/ClienteResource.java
@@ -0,0 +1,32 @@
+package com.leandrolopes.cursomc.resources;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.leandrolopes.cursomc.domain.Cliente;
+import com.leandrolopes.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {			
+		Cliente obj = service.buscar(id);		
+		return ResponseEntity.ok().body(obj);
+	}
+}
+
+
+//ResponseEntity encapsula/armazena varias informacoes de um resposta http para um servico Rest
+//Autowired instancia automaticamente o objeto
+//para saber que o id da url vai vir para o id da variável usa-se a anotacao pathvarible
+// endpoint de um web service é a URL onde seu serviço pode ser acessado por uma aplicação cliente
+//obter dados usa-se get / o endpoint desse metodo agora vai ser /categorias/algum id que for passado
